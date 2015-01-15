6895bc8936769a1416831412c0d5699f425eb679
Sauli Tähkäpää <sauli.tahkapaa@outlook.com>
2015-01-15 15:34:36 +0200

    Update start.sh

--- teamcity-agent/start.sh
+++ teamcity-agent/start.sh
@@ -2,7 +2,9 @@
 
 trap "/opt/agent/bin/agent.sh stop" SIGINT SIGTERM
 /opt/agent/bin/agent.sh start
-while :
+pid=`cat /opt/agent/logs/buildAgent.pid`
+
+while kill -0 $pid 2> /dev/null;
 do
   sleep 1
 done
